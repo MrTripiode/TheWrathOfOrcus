847b13fe1a4e9833eb6daac9b27b558bbea4a418
Antoine Poussard <antoine.poussard@gmail.com>
2020-03-12 14:07:02 +0100

    [FUNCTION] Quest constructor

--- TheWrathOfOrcusTests/QuestsTests.cs
+++ TheWrathOfOrcusTests/QuestsTests.cs
@@ -1,6 +1,7 @@
 ﻿
 using NUnit.Framework;
 using TheWrathOfOrcus;
+using System.Diagnostics;
 
 namespace TheWrathOfOrcusTests
 {
@@ -13,10 +14,14 @@ namespace TheWrathOfOrcusTests
         [Test]
         public void QuestCreation()
         {
-            Quest quest = new Quest("TestQuest", "Awesome Quest to do", 3);
+            Loot loot = new Loot(29, 30);
+            Debug.WriteLine("OUI");
+            Quest quest = new Quest("TestQuest", "Awesome Quest to do", 3, loot);
             Assert.AreEqual(quest.name, "TestQuest");
             Assert.AreEqual(quest.description, "Awesome Quest to do");
             Assert.AreEqual(quest.difficulty, 3);
+            Assert.AreEqual(quest.loot, loot);
+            Assert.AreEqual(quest.monsters.Count, 6);
         }
     }
 }
